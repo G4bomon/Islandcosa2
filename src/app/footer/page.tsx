49e7269fb37195460fb10f3d452b4b12646fe0ae
Javier <jesg60@gmail.com>
2025-01-29 13:04:28 -0400

    margin fix

--- src/app/footer/page.tsx
+++ src/app/footer/page.tsx
@@ -3,11 +3,7 @@ import Image from "next/image";
 const page = () => {
   return (
     <>
-
-
-
-
-      <footer className="bg-amber-400 rounded-lg shadow-sm dark:bg-gray-900 m-4">
+      <footer className="bg-amber-400 rounded-lg shadow-sm dark:bg-gray-900 mt-4">
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
             <div className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
