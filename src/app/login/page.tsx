d21e55c5e0efa6362c9be8075798c3663988c912
Samuel <BCGSS-ADMIN@DESKTOP-GJEDCTP>
2025-02-05 17:17:21 -0400

    captcha

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,94 +1,147 @@
 "use client";
-import { FormEvent, useState } from "react";
-import { AxiosError } from "axios";
+import { FormEvent, useState, useEffect } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
-
-//NO SE USARA
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-
-
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import Link from "next/link"
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import ReCAPTCHA from "react-google-recaptcha";
 
 function Signin() {
+  const [loginAttempt, setLoginAttempt] = useState(0); // 0: no ha intentado, 1: intento fallido
   const [error, setError] = useState("");
+  const [captchaToken, setCaptchaToken] = useState<string | null>(null); // Store the captcha token
   const router = useRouter();
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+  const handleCaptchaChange = (token: string | null) => {
+    setCaptchaToken(token);
+  };
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    setLoginAttempt(loginAttempt + 1); // Incrementa el contador de intentos
+
+    if (!captchaToken) {
+      setError("Por favor, completa el reCAPTCHA.");
+      return; // Stop submission if captcha is not verified
+    }
+
     const formData = new FormData(event.currentTarget);
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    if (!email || !password) {
+      setError("Correo y/o contraseña son requeridos");
+      return;
+    }
+
     const res = await signIn("credentials", {
-      email: formData.get("email"),
-      password: formData.get("password"),
+      email,
+      password,
       redirect: false,
     });
 
-    if (res?.error) setError(res.error as string);
+    if (res?.error) {
+      setError(res.error as string);
+    }
 
-    if (res?.ok) return router.push("/dashboard/profile");
-  };
+    if (res?.ok) {
+      router.push("/dashboard/profile");
+      setCaptchaToken(null); // Restablece el token del captcha
+      setLoginAttempt(0); // Restablece el contador de intentos
+    }
+  }
+
+  useEffect(() => {
+    const storedAttempt = localStorage.getItem("loginAttempt");
+    if (storedAttempt) {
+      setLoginAttempt(parseInt(storedAttempt));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("loginAttempt", loginAttempt.toString());
+  }, [loginAttempt]);
 
   return (
-
-    <>
-      <div className="mt-10 flex flex-col w-11/12 max-w-lg md:w-11/12 mx-auto shadow-lg rounded-lg">
-        <div className="logo-section bg-logo-background bg-cover bg-clip-content grow-0  mb-2 rounded-t-lg">
-          <div>
-            <Image
-              src="/img/logo-round.png"
-              alt="PreDespacho Logo"
-              width={80}
-              height={80}
-              className="mx-auto my-4"
+    <div className="mt-10 flex flex-col w-11/12 max-w-lg md:w-11/12 mx-auto shadow-lg rounded-lg">
+      <div className="bg-logo-background bg-cover bg-clip-content grow-0 mb-2 rounded-t-lg">
+        <Image
+          src="/img/logo-round.png"
+          alt="PreDespacho Logo"
+          width={80}
+          height={80}
+          className="mx-auto my-4"
+        />
+      </div>
+      <div className="p-8">
+        <div className="mb-6">
+          <h1 className="text-3xl">Iniciar Sesión</h1>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col space-y-1.5 mb-3">
+            <Label
+              htmlFor="email"
+              className="block font-semibold text-base mb-1"
+            >
+              Correo electronico
+            </Label>
+            <Input
+              type="email"
+              name="email"
+              className="hover:shadow-md"
+              placeholder="Ingresa tu correo"
+              required // Add required attribute
             />
           </div>
-        </div>
-        <div className="login-section p-8">
-          <div className="login-title mb-6">
-            <h1 className="text-3xl">Iniciar Sesión</h1>
+          <div className="flex flex-col space-y-1.5 mb-3">
+            <Label
+              htmlFor="password"
+              className="block font-semibold text-base mb-1"
+            >
+              Contraseña
+            </Label>
+            <Input
+              type="password"
+              name="password"
+              className="hover:shadow-md"
+              placeholder="Ingresa tu contraseña"
+              required // Add required attribute
+            />
           </div>
-          <form onSubmit={handleSubmit}>
-
-            <div className="login-form">
-              <div className="flex flex-col space-y-1.5 mb-3">
-                <Label htmlFor="email" className="block font-semibold text-base mb-1">Correo electronico</Label>
-                <Input type="email" name="email" className = "hover:shadow-md" placeholder="Ingresa tu correo" />
-              </div>
-              <div className="flex flex-col space-y-1.5 mb-3">
-                <Label htmlFor="password" className="block font-semibold text-base mb-1">Contraseña</Label>
-                <Input type="password" name="password" className="hover:shadow-md" placeholder="Ingresa tu contraseña" />
-              </div>
-              {error && (
-                <div className="flex justify-center mt-4 bg-red-600 text-white rounded-md mb-4"> {error} </div>
-              )}
-              <Button className="flex mt-1 w-full hover:bg-[#FFB200]">Log in</Button>
+          {error && (
+            <div className="flex justify-center mt-4 bg-red-600 text-white rounded-md mb-4 p-2">
+              {error}
             </div>
-          </form>
-          <div className="flex flex-col items-center my-4">
-            <div>
-              <p>
-              ¿No tienes cuenta? {" "}
-              <Link href={"/register"} className="hover:text-blue-500">
-                Registrate
-              </Link>
+          )}
 
-              </p>
+          {loginAttempt > 0 && ( // Muestra el reCAPTCHA solo si hay un intento fallido
+            <div className="mb-4">
+              <ReCAPTCHA
+                sitekey="6Le3Gs4qAAAAAGhdOpHOIxJFhnV1f85ezIGCojgh"
+                onChange={handleCaptchaChange}
+              />
             </div>
-          </div>
+          )}
+
+          <Button type="submit" className="flex w-full hover:bg-[#FFB200]">
+            {" "}
+            {/* Added type="submit" */}
+            Iniciar Sesión
+          </Button>
+        </form>
+        <div className="flex flex-col items-center my-4">
+          <p>
+            ¿No tienes cuenta?{" "}
+            <Link href={"/register"} className="hover:text-blue-500">
+              Registrate
+            </Link>
+          </p>
         </div>
-
       </div>
-      </>
+    </div>
   );
 }
 
