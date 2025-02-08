96516f3442b5bb3c0fa6028f7b85babf01df8c29
Javier <jesg60@gmail.com>
2025-02-08 14:52:28 -0400

    componente moreToDo completo, modificacion de estilo de los carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 interface CarouselProps {
   children: React.ReactNode[]; // Las tarjetas que se mostrarán
@@ -7,8 +8,25 @@ interface CarouselProps {
 
 const Carousel: React.FC<CarouselProps> = ({ children }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const itemsToShow = Math.min(children.length, 3); // Mostrar máximo 3 a la vez
+  const [itemsToShow, setItemsToShow] = useState(3); // Default en desktop
 
+  useEffect(() => {
+    const updateItemsToShow = () => {
+      if (window.innerWidth < 768) {
+        setItemsToShow(Math.min(children.length, 2)); // En móviles, mostrar 2
+      } else {
+        setItemsToShow(Math.min(children.length, 3)); // En desktop, mostrar 3
+      }
+    };
+
+    updateItemsToShow();
+    window.addEventListener("resize", updateItemsToShow);
+    
+    return () => window.removeEventListener("resize", updateItemsToShow);
+  }, [children.length]);
+
+
+  
   const nextSlide = () => {
     if (currentIndex < children.length - itemsToShow) {
       setCurrentIndex(currentIndex + 1);
@@ -26,23 +44,21 @@ const Carousel: React.FC<CarouselProps> = ({ children }) => {
       {/* Botón Anterior */}
       {currentIndex > 0 && (
         <button
-          className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-blue-600 text-white hover:bg-blue-700 p-3 rounded-full shadow-lg z-10"
+          className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-white hover:bg-amber-400 p-3 rounded-full border z-10"
           onClick={prevSlide}
         >
-          ◀
+          <ChevronLeft className="text-black" size={24}/>
         </button>
       )}
 
       {/* Carrusel */}
-      <div className="overflow-hidden border rounded-lg shadow-md">
+      <div className="overflow-hidden">
         <div
           className="flex transition-transform duration-500 ease-in-out"
-          style={{
-            transform: `translateX(-${currentIndex * (100 / itemsToShow)}%)`,
-          }}
+          style={{ transform: `translateX(-${currentIndex * (100 / itemsToShow)}%)` }}
         >
           {children.map((child, index) => (
-            <div key={index} className="w-1/3 flex-shrink-0 p-4">
+            <div key={index} className="md:w-1/3 w-1/2 w- flex-shrink-0 p-4">
               {child}
             </div>
           ))}
@@ -52,10 +68,10 @@ const Carousel: React.FC<CarouselProps> = ({ children }) => {
       {/* Botón Siguiente */}
       {currentIndex < children.length - itemsToShow && (
         <button
-          className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-amber-400 text-white hover:bg-blue-700 p-3 rounded-full shadow-lg z-10"
+          className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-white hover:bg-amber-400 p-3 rounded-full border z-10"
           onClick={nextSlide}
         >
-          ▶
+          <ChevronRight className="text-black" size={24}/>
         </button>
       )}
     </div>
--- src/components/NewsList.tsx
+++ src/components/NewsList.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { useState } from "react";
-import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
 import Carousel from "@/components/Carousel";
 import { useRouter } from "next/navigation";
+
 
 const NewsList: React.FC<{ newsArticles: any[] }> = ({ newsArticles }) => {
   const [categoryFilter, setCategoryFilter] = useState<string>("All");
@@ -28,14 +28,14 @@ const NewsList: React.FC<{ newsArticles: any[] }> = ({ newsArticles }) => {
         <h2 className="font-semibold text-2xl pl-4 pb-2">
           Descubra más cosas que hacer
         </h2>
-        <div className="flex gap-3 flex-wrap">
+        <div className="flex gap-3 flex-wrap px-4">
           {categories.map((category) => (
             <button
               key={category}
               onClick={() => setCategoryFilter(category)}
               className={`px-4 py-2 rounded-xl ${categoryFilter === category
-                  ? "bg-amber-400 text-black font-medium"
-                  : "bg-gray-200 text-black font-medium hover:bg-gray-300"
+                ? "bg-amber-400 text-black font-medium"
+                : "bg-gray-200 text-black font-medium hover:bg-gray-300"
                 }`}
             >
               {category}
@@ -48,19 +48,21 @@ const NewsList: React.FC<{ newsArticles: any[] }> = ({ newsArticles }) => {
       <Carousel>
         {carouselArticles.map((article) => (
           <Link href={`/view/${article._id}/full`} key={article._id}>
-            <div className="border p-4 rounded-lg shadow-lg hover:shadow-2xl transition duration-300 transform hover:scale-105">
+            <div className= "p-4 rounded hover:shadow-lg  transition duration-300 transform hover:scale-105">
               {article.image && (
                 <img
                   src={article.image}
                   alt={article.title}
-                  className="w-full h-64 object-cover mt-4 rounded-lg"
+                  className="w-full h-64 object-cover my-4"
                   loading="lazy"
                 />
               )}
-              <Badge className="mt-2">{article.category}</Badge>
-              <h2 className="text-xl font-semibold text-blue-600">{article.title}</h2>
-              <p className="text-sm text-gray-500">Autor: {article.author || "Desconocido"}</p>
-              <p className="text-sm text-gray-500">{article.content || "No se añadió una descripción"}</p>
+              <p className="text-xs">
+                {article.category}
+              </p>
+              <h2 className="text-2xl font-semibold text-black line-clamp-2">{article.title}</h2>
+              <p className="text-xs text-gray-500 pb-2">Autor: {article.author || "Desconocido"}</p>
+              <p className="text-sm text-gray-500 line-clamp-2">{article.content || "No se añadió una descripción"}</p>
             </div>
           </Link>
         ))}
@@ -68,10 +70,10 @@ const NewsList: React.FC<{ newsArticles: any[] }> = ({ newsArticles }) => {
 
       {/* Botón "Ver más" */}
       {filteredArticles.length > 10 && (
-        <div className="text-center mt-4">
+        <div className="md:mt-2 pl-4">
           <button
             onClick={() => router.push(`/?category=${categoryFilter}`)}
-            className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+            className="px-6 py-3 bg-[rgba(221,234,255,0.5)] text-black font-medium rounded-lg hover:bg-amber-400 transition"
           >
             Ver más
           </button>
