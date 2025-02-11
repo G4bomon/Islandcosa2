272c7c92a848789d38f6da2a3a08b7af49c1db41
G4bomon <gabrielmontesbrea@gmail.com>
2025-02-11 19:55:18 -0400

    hay fe

--- src/app/busqueda/[busqueda]/page.tsx
+++ src/app/busqueda/[busqueda]/page.tsx
@@ -3,6 +3,7 @@ import { Badge } from "@/components/ui/badge";
 import { connectDB } from "@/libs/mongodb";
 import News from "@/models/news";
 import Link from "next/link";
+import Image from "next/image";
 
 interface SearchPageProps {
   params: { busqueda: string };
@@ -13,36 +14,46 @@ export default async function SearchPage({ params }: SearchPageProps) {
 
   await connectDB();
 
-  // Buscar artículos cuyo título contenga la palabra clave
   const articles = await News.find({
-    title: { $regex: searchQuery, $options: "i" }, // Búsqueda insensible a mayúsculas/minúsculas
+    title: { $regex: searchQuery, $options: "i" }, 
   });
 
   if (!articles.length) {
-    return <p className="text-center text-gray-500">No se encontraron artículos para "{searchQuery}".</p>;
+    return (
+      <p className="text-center text-gray-500">
+        No se encontraron artículos para &quot;{searchQuery}&quot;.
+      </p>
+    );
   }
 
   return (
     <div>
-        <BackButton/>
+      <BackButton />
       <h1 className="text-2xl font-bold text-center my-4">
-        Resultados de búsqueda para: "{searchQuery}"
+        Resultados de búsqueda para: &quot;{searchQuery}&quot;
       </h1>
       <div className="grid md:grid-cols-3 gap-4 pt-8">
         {articles.map((article) => (
           <Link href={`/view/${article._id}/full`} key={article._id}>
             <div className="p-4 rounded hover:shadow-lg transition duration-300 transform">
               {article.image && (
-                <img
+                <Image
                   src={article.image}
                   alt={article.title}
+                  width={500} // Ajusta el tamaño según sea necesario
+                  height={300}
                   className="w-full h-64 object-cover"
-                  loading="lazy"
                 />
               )}
-              <Badge className="mt-2 bg-amber-400 text-white">{article.category}</Badge>
-              <h2 className="text-2xl font-semibold text-black line-clamp-2">{article.title}</h2>
-              <p className="text-xs text-gray-500 pb-2">Autor: {article.author || "Desconocido"}</p>
+              <Badge className="mt-2 bg-amber-400 text-white">
+                {article.category}
+              </Badge>
+              <h2 className="text-2xl font-semibold text-black line-clamp-2">
+                {article.title}
+              </h2>
+              <p className="text-xs text-gray-500 pb-2">
+                Autor: {article.author || "Desconocido"}
+              </p>
               <p className="text-sm text-gray-400">
                 Publicado el: {new Date(article.date).toLocaleDateString("es-ES")}
               </p>
